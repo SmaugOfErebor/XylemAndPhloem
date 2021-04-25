8878d2075525726a6e7ee13a616cc03a2189bcea
Justin Nelson <justin.nelson1@snhu.edu>
2021-04-25 16:43:51 -0400

    Randomized the leaf and root sound effects.

--- autoloads/audio.gd
+++ autoloads/audio.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var rng = RandomNumberGenerator.new()
+
 onready var audioPlayers: Node = get_tree().get_root().get_node("root_node").get_node("audio_players")
 
 onready var leafSoundPlayer: AudioStreamPlayer = audioPlayers.get_node("leaf_sound_player")
@@ -8,10 +10,12 @@ onready var snipSoundPlayer: AudioStreamPlayer = audioPlayers.get_node("snip_sou
 
 func playLeafSound():
 	if !snipSoundPlayer.is_playing():
+		leafSoundPlayer.pitch_scale = rng.randf_range(0.95, 1.05)
 		leafSoundPlayer.play()
 
 func playRootSound():
 	if !snipSoundPlayer.is_playing():
+		rootSoundPlayer.pitch_scale = rng.randf_range(0.7, 1.0)
 		rootSoundPlayer.play()
 
 func playSnipSound():
